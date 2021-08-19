cca5c2168b2009117ee1a2d36424828c9089d2fd
cemaltuysuz <ctuysuz18@posta.pau.edu.tr>
2021-08-19 12:36:51 +0300

    created project

---
+++ twitter-navcontroller-example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  twitter-navcontroller-example
+//
+//  Created by cemal tüysüz on 19.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ twitter-navcontroller-exampleTests/twitter_navcontroller_exampleTests.swift
@@ -0,0 +1,33 @@
+//
+//  twitter_navcontroller_exampleTests.swift
+//  twitter-navcontroller-exampleTests
+//
+//  Created by cemal tüysüz on 19.08.2021.
+//
+
+import XCTest
+@testable import twitter_navcontroller_example
+
+class twitter_navcontroller_exampleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
